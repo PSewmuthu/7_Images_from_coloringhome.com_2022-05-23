acaf68d70c07d493c65faeb0174bb9ac484ec1f3
PSewmuthu <apasindusewmuthu@gmail.com>
2022-05-23 16:18:21 +0530

    Fixed a error

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from bs4 import BeautifulSoup
 import requests
 import os
 import shutil
+import time
 
 
 def get_links():
@@ -18,9 +19,9 @@ def get_links():
         header_list = []
         for header in soup.find_all('h2', {"class": "ctitle"}):
             if '&amp;' in header.string:
-                header_list.append(header.string.replace('&amp;', '&'))
+                header_list.append(header.string.replace('&amp;', '&').strip())
             else:
-                header_list.append(header.string)
+                header_list.append(header.string.strip())
 
         link_list = {}
         attrs = soup.find_all('div', {"class": "catts"})
@@ -29,7 +30,7 @@ def get_links():
             for div in attrs[header_list.index(header)].find_all("div"):
                 try:
                     a = div.find('p').find('a')
-                    links[f"https://coloringhome.com{a['href']}"] = a.string
+                    links[f"https://coloringhome.com{a['href']}"] = a.string.strip()
                 except:
                     pass
 
@@ -51,42 +52,46 @@ def greb_images():
                     print(f"Fetching from {link}")
                     os.makedirs(f"img/{header}/{name}", exist_ok=True)
 
-                    html = requests.get(link).text
-                    soup = BeautifulSoup(html, 'lxml')
-                    images = soup.find_all(
-                        'div', {"class": "thumbnail side-corner-tag"})
+                    try:
+                        html = requests.get(link).text
+                        soup = BeautifulSoup(html, 'lxml')
+                        images = soup.find_all(
+                            'div', {"class": "thumbnail side-corner-tag"})
 
-                    i = 1
-                    for img in images:
-                        url = f"https://coloringhome.com{img.find('a').find('img')['src']}"
+                        i = 1
+                        for img in images:
+                            url = f"https://coloringhome.com{img.find('a').find('img')['src']}"
 
-                        ext = url[url.rindex('.'):]
-                        if ext.startswith('.png'):
-                            ext = '.png'
-                        elif ext.startswith('.jpg'):
-                            ext = '.jpg'
-                        elif ext.startswith('.jfif'):
-                            ext = '.jfif'
-                        elif ext.startswith('.com'):
-                            ext = '.jpg'
-                        elif ext.startswith('.svg'):
-                            ext = '.svg'
+                            ext = url[url.rindex('.'):]
+                            if ext.startswith('.png'):
+                                ext = '.png'
+                            elif ext.startswith('.jpg'):
+                                ext = '.jpg'
+                            elif ext.startswith('.jfif'):
+                                ext = '.jfif'
+                            elif ext.startswith('.com'):
+                                ext = '.jpg'
+                            elif ext.startswith('.svg'):
+                                ext = '.svg'
 
-                        j = ''
-                        if len(str(i)) == 1:
-                            j = f"00{i}"
-                        elif len(str(i)) == 2:
-                            j = f"0{i}"
-                        else:
-                            j = str(i)
+                            j = ''
+                            if len(str(i)) == 1:
+                                j = f"00{i}"
+                            elif len(str(i)) == 2:
+                                j = f"0{i}"
+                            else:
+                                j = str(i)
 
-                        data = requests.get(url, stream=True)
-                        filename = f"{name}.{str(j)}{ext}"
+                            data = requests.get(url, stream=True)
+                            filename = f"{name}.{str(j)}{ext}"
 
-                        with open(f"img/{header}/{name}/{filename}", 'wb') as file:
-                            shutil.copyfileobj(data.raw, file)
+                            with open(f"img/{header}/{name}/{filename}", 'wb') as file:
+                                shutil.copyfileobj(data.raw, file)
 
-                        i += 1
+                            i += 1
+                    except:
+                        pass
+                    time.sleep(3)
         else:
             return "Error"
     except:
