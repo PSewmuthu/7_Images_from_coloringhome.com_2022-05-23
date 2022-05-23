66226ac149220b4e91fd5ac96b84235d4e6338ac
PSewmuthu <apasindusewmuthu@gmail.com>
2022-05-23 10:29:38 +0530

    Added get_links() function

--- main.py
+++ main.py
@@ -8,3 +8,22 @@ from bs4 import BeautifulSoup
 import requests
 import os
 import shutil
+
+
+def get_links():
+    html = requests.get('https://coloringhome.com/categories').text
+    soup = BeautifulSoup(html, 'lxml')
+
+    header_list = []
+    for header in soup.find_all('h2', {"class": "ctitle"}):
+        header_list.append(header.string.replace('&amp;', '&'))
+
+    link_list = {}
+    for div in soup.find_all('div', {"class": "catts"}):
+        attrs = soup.find_all('div', {"class": "catts"})
+        for header in header_list:
+            links = {}
+            for a in attrs[header_list.index(header)].find("div", {"class": "catt catred"}).find("p").find("a"):
+                links[f"https://coloringhome.com{a['href']}"] = a.string
+
+            link_list[header] = links
